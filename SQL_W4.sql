d895ece59b7350b6f31c54c085fb762c0630c6c2
batuhanmur <91535620+batuhanmur@users.noreply.github.com>
2021-10-04 11:58:36 +0300

    Create SQL_W4.sql

---
+++ SQL_W4.sql
@@ -0,0 +1,5 @@
+--SELECT DISTINCT replacement_cost from film;
+--SELECT COUNT(DISTINCT replacement_cost) from film;
+--SELECT COUNT(title) from film where title LIKE 'T%' and rating='G';
+--SELECT COUNT(country) from country where country LIKE '_____';
+--SELECT COUNT(city) from city where city ILIKE '%R';

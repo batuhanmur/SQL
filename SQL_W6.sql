be92af42f3c2d9104ec211c7e04f4ff21728d9d9
batuhanmur <91535620+batuhanmur@users.noreply.github.com>
2021-10-05 22:33:31 +0300

    Create SQL_W6.sql

---
+++ SQL_W6.sql
@@ -0,0 +1,4 @@
+--select ROUND(AVG(rental_rate),2) from film;
+--select count(*) from film where title LIKE 'C%';
+--select max(length) from film where rental_rate= 0.99;
+--select count(replacement_cost) from film where length>150;

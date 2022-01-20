9627adb2162d67e7fb17e5709aa08f50d30680a7
batuhanmur <91535620+batuhanmur@users.noreply.github.com>
2022-01-20 11:21:44 +0300

    Create SQL_W10.sql

---
+++ SQL_W10.sql
@@ -0,0 +1,8 @@
+--select city, country from city
+--left join country on city.country_id=country.country_id;
+
+--select first_name, last_name from customer
+--right join payment on customer.customer_id=payment.customer_id;
+
+--select first_name, last_name from customer
+--full join rental on rental.customer_id=customer.customer_id;
